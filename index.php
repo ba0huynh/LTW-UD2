55c59f175d296402d3325cff2b84d79b37889bdd
nhieelam <lam.nhiee11@gmail.com>
2025-03-15 08:51:33 +0700

    header

--- index.php
+++ index.php
@@ -14,13 +14,16 @@ require_once("./database/database.php");
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    
     <!-- custom css -->
     <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="./css/header.css">
+
 </head>
 
 <body>
 <?php include_once "./zui/login.php" ?>
+<?php include_once "./zui/header.php" ?>
 
 <?php include_once "./zui/footer.php" ?>
 </body>
