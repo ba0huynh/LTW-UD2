acb09afd5ee2d1a52b5b02831082ec98dd771e88
Hienhehehe <phuochienphan097@gmail.com>
2025-03-21 13:17:57 +0700

    .

---
+++ zui/suggested_book.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Store</title>
+    <link rel="stylesheet" href="../css/suggested_book.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
+</head>
+
+<body>
+
+
+    <div class="wrapped">
+
+        <div class="container">
+            <div class="Suggesting">
+                <h1 id="section">
+                    <div class="icon-overlay"></div>
+                    <i class='bx bxs-book-open' style='color:#f9f6f6'></i>
+                    Gợi ý cho bạn
+                </h1>
+            </div>
+
+
+            <div class="item">
+                <img src="https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg"
+                    alt="Book">
+                <h3 class="book-name">Tự Chữa Lành Thông Qua Hiểu Biết</h3>
+                <div class="price-container">
+                    <div class="discount-price">89.250 đ</div>
+                    <span class="giam-gia">-25%</span>
+                </div>
+                <div class="original-price">119.000 đ</div>
+                <p class="sold">Đã bán 102</p>
+            </div>
+
+            <div class="item">
+                <img src="https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg"
+                    alt="Book">
+                <h3 class="book-name">Tự Chữa Lành Thông Qua Hiểu Biết</h3>
+                <div class="price-container">
+                    <div class="discount-price">89.250 đ</div>
+                    <span class="giam-gia">-25%</span>
+                </div>
+                <div class="original-price">119.000 đ</div>
+                <p class="sold">Đã bán 102</p>
+            </div>
+
+            <div class="item">
+                <img src="https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg"
+                    alt="Book">
+                <h3 class="book-name">Tự Chữa Lành Thông Qua Hiểu Biết</h3>
+                <div class="price-container">
+                    <div class="discount-price">89.250 đ</div>
+                    <span class="giam-gia">-25%</span>
+                </div>
+                <div class="original-price">119.000 đ</div>
+                <p class="sold">Đã bán 102</p>
+            </div>
+
+            <div class="item">
+                <img src="https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg"
+                    alt="Book">
+                <h3 class="book-name">Tự Chữa Lành Thông Qua Hiểu Biết</h3>
+                <div class="price-container">
+                    <div class="discount-price">89.250 đ</div>
+                    <span class="giam-gia">-25%</span>
+                </div>
+                <div class="original-price">119.000 đ</div>
+                <p class="sold">Đã bán 102</p>
+            </div>
+            <div class="item">
+                <img src="https://salt.tikicdn.com/cache/280x280/ts/product/22/cb/a9/524a27dcd45e8a13ae6eecb3dfacba7c.jpg"
+                    alt="Book">
+                <h3 class="book-name">Tự Chữa Lành Thông Qua Hiểu Biết</h3>
+                <div class="price-container">
+                    <div class="discount-price">89.250 đ</div>
+                    <span class="giam-gia">-25%</span>
+                </div>
+                <div class="original-price">119.000 đ</div>
+                <p class="sold">Đã bán 102</p>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
