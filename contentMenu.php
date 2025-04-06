33877fcdbef2ca86359c2cbeba992ea2a5e3fdc8
nhieelam <lam.nhiee11@gmail.com>
2025-04-07 02:58:28 +0700

    .

--- contentMenu.php
+++ contentMenu.php
@@ -4,7 +4,6 @@ require_once("./database/database.php");
 $servername="localhost";
 $username="root";
 $password="";
-$dbname="ltw&ud2";
 $dbname="ltw_ud2";
 $conn=new mysqli($servername,$username,$password,$dbname);
 ?>    
