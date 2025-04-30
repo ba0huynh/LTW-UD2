f38eefd5c48a608208df7f75df765e86b91a592b
nhieelam <lam.nhiee11@gmail.com>
2025-04-30 09:09:10 +0700

    naruto2

--- components/logout.php
+++ components/logout.php
@@ -1,8 +1,13 @@
 <?php
 session_start();
+if(!isset($_GET["user_id"])){
+    header("Location: /LTW_UD2/");
+}
 session_unset(); 
-session_destroy(); 
-
-header("Location: ../");
-exit();
+session_destroy();
 ?>
+
+<script>
+alert("Tài khoản đã đăng xuất");
+window.location.href = "../"; // chuyển trang bằng JavaScript luôn
+</script>
